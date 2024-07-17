87a2586d5065accb55af3b23ee7f85a7ae722be3
JAMES_BOND_OO7 <99027390+RupeshSangar@users.noreply.github.com>
2024-07-17 17:10:49 +0000

    learn variable declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accId = 23;
+let accEmail = "rupesh@gmail.com";
+var accName = "Rupesh";
+accCity = "mumbai"
+
+accEmail = "billionaireRupesh@dollors.com"
+
+/*
+we do not prefer to use var 
+because of issue in block scope and functional scope
+*/
+
+
+
+console.log(accId);
+
+console.table([accEmail, accName, accCity])
