342b660e2d679346cde398eb42fc77c5b6e22bc8
JAMES_BOND_OO7 <99027390+RupeshSangar@users.noreply.github.com>
2024-07-18 16:57:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3 ) //we are using nodejs , not browser
+
+let name = "Rupesh";
+let age = 22;
+let state = null;
+
+console.table([typeof state , typeof name , typeof age , typeof undefined])
